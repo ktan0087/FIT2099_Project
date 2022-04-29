9bc38b2face4ff863757facf81caabc194ddd2e1
dong0009 <dong0009@student.monash.edu>
2022-04-29 16:59:13 +0800

    Override resetInstance method (REQ7)

--- src/game/grounds/Tree.java
+++ src/game/grounds/Tree.java
@@ -1,8 +1,11 @@
 package game.grounds;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.enums.Status;
+import game.interfaces.Resettable;
 
-public class Tree extends Ground {
+public class Tree extends Ground implements Resettable {
 
     /**
      * Constructor.
@@ -10,5 +13,18 @@ public class Tree extends Ground {
      */
     public Tree() {
         super('+');
+        this.registerInstance();
+    }
+
+    @Override
+    public void tick(Location location) {
+        if (this.hasCapability(Status.RESET) && Math.random() > 0.5){
+            location.setGround(new Dirt());
+        }
+    }
+
+    @Override
+    public void resetInstance() {
+        this.addCapability(Status.RESET);
     }
 }
