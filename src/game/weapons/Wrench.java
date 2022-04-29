b079dea85121ca91384a969b9be64dad83e0b2ac
dong0009 <dong0009@student.monash.edu>
2022-04-29 17:00:03 +0800

    Create a Wrench class

---
+++ src/game/weapons/Wrench.java
@@ -0,0 +1,25 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+import game.interfaces.Tradable;
+
+public class Wrench extends WeaponItem implements Tradable {
+    private static final int PRICE = 200;
+    private static final int HIT_RATE = 80;
+    private static final int DAMAGE = 50;
+    private static final char WRENCH_CHAR = 'w';
+    private static final String WRENCH_VERB = "whacks";
+
+    /**
+     * Constructor.
+     *
+     */
+    public Wrench() {
+        super("Wrench", WRENCH_CHAR, DAMAGE, WRENCH_VERB, HIT_RATE);
+    }
+
+    @Override
+    public int getPrice() {
+        return PRICE;
+    }
+}
