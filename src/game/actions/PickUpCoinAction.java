cfce21e3bfe53a6e2dfa5e47ad878f8622afee86
dong0009 <dong0009@student.monash.edu>
2022-04-29 16:41:10 +0800

    Create a PickUpCoinAction class to pick up a Coin item on the Ground

---
+++ src/game/actions/PickUpCoinAction.java
@@ -0,0 +1,34 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.Wallet;
+import game.items.Coin;
+
+public class PickUpCoinAction extends Action {
+
+    private Coin coin;
+
+    /**
+     * Constructor.
+     *
+     * @param coin the coin to pick up
+     */
+    public PickUpCoinAction(Coin coin) {
+        this.coin = coin;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        Wallet.addBalance(coin.getValue());
+        map.locationOf(actor).removeItem(coin);
+        return menuDescription(actor);
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " picks up a $" + coin.getValue() + " coin";
+    }
+}
+
