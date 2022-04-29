e6df0d3823fbf1f95e1f6a9c31f28c485bf688bc
dong0009 <dong0009@student.monash.edu>
2022-04-29 19:42:41 +0800

    Added speaking related methods

--- src/game/actions/SpeakAction.java
+++ src/game/actions/SpeakAction.java
@@ -2,25 +2,61 @@ package game.actions;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.enums.Status;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class SpeakAction extends Action {
-    private String message;
     private Actor target;
+    private static final int EXCLUDE_WRENCH_MSG = 0;
+    private static final int EXCLUDE_POWER_STAR_MSG = 1;
+    private static final String[] TOAD_MSG = {"You might need a wrench to smash Koopa's hard shells.",
+                                              "You better get back to finding the Power Stars.",
+                                              "The Princess is depending on you! You are our only hope.",
+                                              "Being imprisoned in these walls can drive a fungus crazy :("};
 
-    public SpeakAction(String message, Actor target){
-        this.message = message;
+    public SpeakAction(Actor target){
         this.target = target;
     }
 
     @Override
     public String execute(Actor actor, GameMap map) {
-        return target + ": " + message;
+        ArrayList<Integer> excludeMsgIndexList = new ArrayList<>();
+
+        // check for wrench in inventory
+        for (Item item : actor.getInventory()){
+            if (item.hasCapability(Status.CAN_SMASH_KOOPA_SHELL)){
+                excludeMsgIndexList.add(EXCLUDE_WRENCH_MSG);
+                break;
+            }
+        }
+
+        // check power star effect
+        if (actor.hasCapability(Status.INVINCIBLE)){
+            excludeMsgIndexList.add(EXCLUDE_POWER_STAR_MSG);
+        }
+
+        return getRandomMsg(excludeMsgIndexList);
     }
 
     @Override
     public String menuDescription(Actor actor) {
         return actor + " talks with " + target;
+    }
+
+    private String getRandomMsg(ArrayList<Integer> excludeMsgIndexList){
+        ArrayList<String> msg = new ArrayList<>();
+
+        for (int i = 0; i < TOAD_MSG.length; i++){
+            if (!(excludeMsgIndexList.contains(i))){
+                msg.add(TOAD_MSG[i]);
+            }
+        }
+
+        return msg.get(new Random().nextInt(msg.size()));
     }
 
 }
