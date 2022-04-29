1e5634db11ae38cf465189254781b15e35fb05c5
dong0009 <dong0009@student.monash.edu>
2022-04-29 16:49:09 +0800

    Added new values

--- src/game/enums/Status.java
+++ src/game/enums/Status.java
@@ -7,4 +7,8 @@ package game.enums;
 public enum Status {
     HOSTILE_TO_ENEMY, // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
     TALL, // use this status to tell that current instance has "grown".
+    INVINCIBLE,
+    UNDROPPABLE,
+    CAN_SMASH_KOOPA_SHELL,
+    RESET
 }
