7f8cead3d90011c644a98950120220da857f56f4
dong0009 <dong0009@student.monash.edu>
2022-04-29 16:42:01 +0800

    Override resetInstance method

--- src/game/actors/Player.java
+++ src/game/actors/Player.java
@@ -6,12 +6,14 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.displays.Menu;
+import game.actions.ResetAction;
 import game.enums.Status;
+import game.interfaces.Resettable;
 
 /**
  * Class representing the Player.
  */
-public class Player extends Actor  {
+public class Player extends Actor implements Resettable {
 
 	private final Menu menu = new Menu();
 
@@ -25,10 +27,15 @@ public class Player extends Actor  {
 	public Player(String name, char displayChar, int hitPoints) {
 		super(name, displayChar, hitPoints);
 		this.addCapability(Status.HOSTILE_TO_ENEMY);
+		this.registerInstance();
 	}
 
 	@Override
 	public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+		if (!(ResetAction.getResetFlag())){
+			actions.add(new ResetAction());
+		}
+
 		// Handle multi-turn Actions
 		if (lastAction.getNextAction() != null)
 			return lastAction.getNextAction();
@@ -41,4 +48,19 @@ public class Player extends Actor  {
 	public char getDisplayChar(){
 		return this.hasCapability(Status.TALL) ? Character.toUpperCase(super.getDisplayChar()): super.getDisplayChar();
 	}
+
+	@Override
+	public void resetInstance() {
+		if (this.hasCapability(Status.TALL)) {
+			this.setDisplayChar(Character.toLowerCase(super.getDisplayChar()));
+			this.removeCapability(Status.TALL);
+		}
+
+		if (this.hasCapability(Status.INVINCIBLE)) {
+			this.removeCapability(Status.INVINCIBLE);
+		}
+
+		this.resetMaxHp(this.getMaxHp());
+	}
+
 }
