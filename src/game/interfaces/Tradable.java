7f51a66e40e2a4f50bef718e8e57476036bd1844
dong0009 <dong0009@student.monash.edu>
2022-04-29 21:13:14 +0800

    Added a default registerTradableInstance method

--- src/game/interfaces/Tradable.java
+++ src/game/interfaces/Tradable.java
@@ -1,6 +1,11 @@
 package game.interfaces;
 
+import game.managers.TradableManager;
+
 public interface Tradable {
     public int getPrice();
 
+    default void registerTradableInstance(){
+        TradableManager.getInstance().addTradableInstance(this);
+    }
 }
