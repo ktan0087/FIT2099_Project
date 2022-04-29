5a0d7750aa33643ee8f5cae1f59d58de9d04df2f
dong0009 <dong0009@student.monash.edu>
2022-04-29 16:52:01 +0800

    Create a SuperMushroom class and override getDropAction & getPrice methods from Item class & Tradable interface respectively

---
+++ src/game/items/SuperMushroom.java
@@ -0,0 +1,31 @@
+package game.items;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.DropItemAction;
+import edu.monash.fit2099.engine.items.Item;
+import game.enums.Status;
+import game.interfaces.Tradable;
+
+public class SuperMushroom extends Item implements Tradable {
+    private static final int PRICE = 400;
+    private static final char SUPER_MUSHROOM_CHAR = '^';
+
+    /***
+     * Constructor.
+     */
+    public SuperMushroom() {
+        super("Super Mushroom", SUPER_MUSHROOM_CHAR, true);
+    }
+
+    @Override
+    public int getPrice() {
+        return PRICE;
+    }
+
+    @Override
+    public DropItemAction getDropAction(Actor actor) {
+        if(this.hasCapability(Status.UNDROPPABLE))
+            return null;
+        return new DropItemAction(this);
+    }
+}
