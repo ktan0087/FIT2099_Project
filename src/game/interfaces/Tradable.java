ee246a4c628e82b7f835d9d93c0f21e24f93f256
dong0009 <dong0009@student.monash.edu>
2022-04-30 07:06:12 +0800

    Added an addCapabilityDuringTrading method signature

--- src/game/interfaces/Tradable.java
+++ src/game/interfaces/Tradable.java
@@ -1,11 +1,8 @@
 package game.interfaces;
 
-import game.managers.TradableManager;
 
 public interface Tradable {
     public int getPrice();
 
-    default void registerTradableInstance(){
-        TradableManager.getInstance().addTradableInstance(this);
-    }
+    public void addCapabilityDuringTrading();
 }
