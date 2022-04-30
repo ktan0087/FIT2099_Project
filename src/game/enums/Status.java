d58539dff8da35f69d5948bc07a884827581b74e
Kennedy Tan <ktan0087@student.monash.edu>
2022-04-30 10:14:46 +0800

    add on enum status class

--- src/game/enums/Status.java
+++ src/game/enums/Status.java
@@ -6,9 +6,12 @@ package game.enums;
  */
 public enum Status {
     HOSTILE_TO_ENEMY, // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
+    HOSTILE_TO_PLAYER, // can be attack by player
     TALL, // use this status to tell that current instance has "grown".
     INVINCIBLE,
     UNDROPPABLE,
     CAN_SMASH_KOOPA_SHELL,
+    DORMANT,
+    AGGRO, //aggressive behaviour
     RESET
 }
