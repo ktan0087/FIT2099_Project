30b58d7738a0920391b6157ce9676e70173c6bcd
Kennedy Tan <ktan0087@student.monash.edu>
2022-04-30 10:16:12 +0800

    update Power Star and SuperMushroom classes

--- src/game/items/PowerStar.java
+++ src/game/items/PowerStar.java
@@ -2,20 +2,20 @@ package game.items;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.DropItemAction;
-import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.Location;
 import game.enums.Status;
 import game.interfaces.Tradable;
 
-public class PowerStar extends Item implements Tradable {
+public class PowerStar extends ConsumableItem implements Tradable {
     private static final int PRICE = 600;
-    private static final String NAME = "Power Star";
     private static final char POWER_STAR_CHAR = '*';
+    private int turn = 10;
 
     /***
      * Constructor.
      */
     public PowerStar() {
-        super(NAME, POWER_STAR_CHAR, true);
+        super("Power Star", POWER_STAR_CHAR, true);
     }
 
     @Override
@@ -33,5 +33,32 @@ public class PowerStar extends Item implements Tradable {
         if(this.hasCapability(Status.UNDROPPABLE))
             return null;
         return new DropItemAction(this);
+    }
+
+    @Override
+    public void tick(Location currentLocation, Actor actor){
+        turn -= 1;
+        if (turn < 1){
+            actor.removeItemFromInventory(this);
+        }
+    }
+
+    @Override
+    public void tick(Location currentLocation){
+        turn -= 1;
+        if (turn < 1){
+            currentLocation.removeItem(this);
+        }
+    }
+
+    @Override
+    public void consumed(Actor consumedActor) {
+        consumedActor.addCapability(Status.INVINCIBLE);
+        consumedActor.heal(200);
+    }
+
+    @Override
+    public String toString(){
+        return "Power Star";
     }
 }
--- src/game/items/SuperMushroom.java
+++ src/game/items/SuperMushroom.java
@@ -2,20 +2,18 @@ package game.items;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.DropItemAction;
-import edu.monash.fit2099.engine.items.Item;
 import game.enums.Status;
 import game.interfaces.Tradable;
 
-public class SuperMushroom extends Item implements Tradable {
+public class SuperMushroom extends ConsumableItem implements Tradable {
     private static final int PRICE = 400;
-    private static final String NAME = "Super Mushroom";
     private static final char SUPER_MUSHROOM_CHAR = '^';
 
     /***
      * Constructor.
      */
     public SuperMushroom() {
-        super(NAME, SUPER_MUSHROOM_CHAR, true);
+        super("Super Mushroom", SUPER_MUSHROOM_CHAR, true);
     }
 
     @Override
@@ -33,5 +31,16 @@ public class SuperMushroom extends Item implements Tradable {
         if(this.hasCapability(Status.UNDROPPABLE))
             return null;
         return new DropItemAction(this);
+    }
+
+    @Override
+    public void consumed(Actor consumedActor) {
+        consumedActor.addCapability(Status.TALL);
+        consumedActor.increaseMaxHp(50);
+    }
+
+    @Override
+    public String toString(){
+        return "SuperMushroom";
     }
 }
