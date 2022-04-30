dae07109f1b44790516b5cdeb6df587fd3f2a581
Kennedy Tan <ktan0087@student.monash.edu>
2022-04-30 10:15:40 +0800

    display message when Mario is invincible

--- src/game/actors/Player.java
+++ src/game/actors/Player.java
@@ -4,8 +4,8 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
-import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.displays.Menu;
+import edu.monash.fit2099.engine.positions.GameMap;
 import game.Wallet;
 import game.actions.ResetAction;
 import game.enums.Status;
@@ -17,6 +17,7 @@ import game.interfaces.Resettable;
 public class Player extends Actor implements Resettable {
 
 	private final Menu menu = new Menu();
+	private int turn = 10;
 
 	/**
 	 * Constructor.
@@ -33,11 +34,25 @@ public class Player extends Actor implements Resettable {
 
 	@Override
 	public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+
 		display.println(this + "'s current balance: $" + Wallet.getBalance());
 
 		if (!(ResetAction.getResetFlag())){
 			actions.add(new ResetAction());
 		}
+
+		if (hasCapability(Status.INVINCIBLE)){
+			display.println("Mario is INVINCIBLE!");
+			display.println("Mario consumes Power Star - " + turn +" turns remaining");
+
+			turn -= 1;
+		}
+
+		if (turn < 1){
+			removeCapability(Status.INVINCIBLE);
+		}
+
+		//add on here
 
 		// Handle multi-turn Actions
 		if (lastAction.getNextAction() != null)
