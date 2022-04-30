1f54782fc00bc23db20138e6337e50e47cb224bd
Kennedy Tan <ktan0087@student.monash.edu>
2022-04-30 10:18:23 +0800

    add FollowBehaviour into AttackBehaviour

--- src/game/behaviours/AttackBehaviour.java
+++ src/game/behaviours/AttackBehaviour.java
@@ -2,14 +2,37 @@ package game.behaviours;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.AttackAction;
+import game.enums.Status;
 import game.interfaces.Behaviour;
 
 public class AttackBehaviour implements Behaviour {
 
+    private Actor followedTarget;
+
     // TODO: develop and use it to attack the player automatically.
     @Override
     public Action getAction(Actor actor, GameMap map) {
+
+        for(Exit exit : map.locationOf(actor).getExits()){
+            Location destination = exit.getDestination();
+            if (destination.containsAnActor()){
+                Actor target = destination.getActor();
+                if (target.hasCapability(Status.HOSTILE_TO_ENEMY) && !actor.hasCapability(Status.HOSTILE_TO_ENEMY) || !target.hasCapability(Status.HOSTILE_TO_ENEMY) && actor.hasCapability(Status.HOSTILE_TO_ENEMY)){
+                    followedTarget = target;
+                    actor.addCapability(Status.AGGRO);
+                    return new AttackAction(target, exit.getName());
+                }
+            }
+        }
+
+        if (followedTarget != null && actor.hasCapability(Status.AGGRO)){
+            return new FollowBehaviour(followedTarget).getAction(actor, map);
+        }
+
         return null;
     }
 }
