0fd06b65e43aedd7357809fc763b5b61dc374589
Kennedy Tan <ktan0087@student.monash.edu>
2022-05-01 08:41:24 +0800

    add DormantKoopa class

---
+++ src/game/enemies/DormantKoopa.java
@@ -0,0 +1,49 @@
+package game.enemies;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.AttackAction;
+import game.enums.Status;
+import game.items.SuperMushroom;
+
+public class DormantKoopa extends Enemy {
+
+    public DormantKoopa(Location location){
+        super("DormantKoopa", 'D', 1, location);
+    }
+
+    @Override
+    public void hurt(int points){
+        super.hurt(points);
+        if (this.hasCapability(Status.DESTROYED)){
+            dropSuperMushroom();
+        }
+    }
+
+    public void dropSuperMushroom(){
+        if (spawnLocation != null){
+            spawnLocation.addItem(new SuperMushroom());
+            spawnLocation.map().removeActor(this);
+        }
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        for (Item item : otherActor.getInventory()){
+            if (item.hasCapability(Status.CAN_SMASH_KOOPA_SHELL)){
+                this.addCapability(Status.DESTROYED);
+                otherActor.hasCapability(Status.CAN_SMASH_KOOPA_SHELL);
+            }
+        }
+
+        if(otherActor.hasCapability(Status.CAN_SMASH_KOOPA_SHELL)) {
+            actions.add(new AttackAction(this,direction));
+        }
+        return actions;
+    }
+
+}
