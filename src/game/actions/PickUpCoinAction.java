1c82c7d21b589d49f87facd408f4fec739c61b4f
dong0009 <dong0009@student.monash.edu>
2022-05-01 08:01:17 +0800

    Added javadoc

--- src/game/actions/PickUpCoinAction.java
+++ src/game/actions/PickUpCoinAction.java
@@ -7,8 +7,17 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import game.Wallet;
 import game.items.Coin;
 
+/**
+ * A PickUpCoinAction class which enables the player to pick up a coin
+ *
+ * @author Ong Di Sheng
+ * @version 1.0
+ */
 public class PickUpCoinAction extends PickUpItemAction {
 
+    /**
+     * The coin to pick up
+     */
     private Coin coin;
 
     /**
@@ -21,13 +30,26 @@ public class PickUpCoinAction extends PickUpItemAction {
         this.coin = coin;
     }
 
+    /**
+     * Picking up a coin from the location and add money to the player's balance
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return a brief description on picking up a coin of a certain value
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
-        Wallet.addBalance(coin.getValue());
-        map.locationOf(actor).removeItem(coin);
+        Wallet.addBalance(coin.getValue()); // add money to the player's balance
+        map.locationOf(actor).removeItem(coin); // remove coin from the location
         return menuDescription(actor);
     }
 
+    /**
+     * Display the message of picking up the coin in the menu
+     *
+     * @param actor The actor performing the action.
+     * @return the message of picking up the coin
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " picks up a $" + coin.getValue() + " coin";
