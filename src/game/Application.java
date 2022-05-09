48839ad69dbc8139b483b426fcdeae86e063c4f8
mman0015 <P-gzdB4NpmtjWSNQFFz3>
2022-05-09 22:25:37 +0800

    Added second map and refactored mechanism for initial random spawning of grounds

--- src/game/Application.java
+++ src/game/Application.java
@@ -7,16 +7,13 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.FancyGroundFactory;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.World;
 import game.actors.Player;
 import game.actors.Toad;
-import game.enemies.Goomba;
-import game.enemies.Koopa;
 import game.grounds.*;
-import game.items.Coin;
 import game.items.PowerStar;
 import game.items.SuperMushroom;
-import game.weapons.Wrench;
 
 /**
  * The main class for the Mario World game.
@@ -28,7 +25,7 @@ public class Application {
 
 			World world = new World(new Display());
 
-			FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor(), new Sprout());
+			FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor(), new Sprout(), new Lava(), new WarpPipe());
 
 			List<String> map = Arrays.asList(
 				"..........................................##..........+.........................",
@@ -51,17 +48,38 @@ public class Application {
 				"......................................................#.........................",
 				".......................................................##.......................");
 
-			addSprouts(map);
+			List<String> lavaMap = Arrays.asList(
+					"C___#....+.........#",
+					"___...............#.",
+					"__#..........+...#..",
+					"_#..............#...",
+					"#..............#....",
+					".......+......#.....",
+					".............#......",
+					"............#...+...",
+					"...+.......#........",
+					"..........#.........");
 
+			// Randomly populate map with grounds
+			generateRandomGrounds(map, new Sprout(), 0.01);
+			generateRandomGrounds(map, new WarpPipe(), 0.01);
+			generateRandomGrounds(lavaMap, new Sprout(), 0.01);
+			generateRandomGrounds(lavaMap, new Lava(), 0.1);
+
+			// Create game maps and add them to the world
 			GameMap gameMap = new GameMap(groundFactory, map);
+			GameMap lavaZone = new GameMap(groundFactory, lavaMap);
 			world.addGameMap(gameMap);
+			world.addGameMap(lavaZone);
 
+			// Create player and place him in the world
 			Actor mario = new Player("Player", 'm', 100);
 			world.addPlayer(mario, gameMap.at(42, 10));
 
-		/**
-		 * Add actor and items in map
-		 */
+			// Assign master warp in second map
+			WarpPipe.setMasterWarp(lavaZone.at(0,0), lavaZone);
+
+			// Add actor and items in map
 			gameMap.at(42,11).addActor(new Toad());
 			gameMap.at(43,10).addItem(new SuperMushroom());
 			gameMap.at(41,10).addItem(new PowerStar());
@@ -69,21 +87,21 @@ public class Application {
 
 	}
 
-
 	/**
-	 * Place some sprouts randomly in map
-	 *
-	 * @param map current game map
+	 * Randomly populate map with selected ground
+	 * @param map The string representation of the map
+	 * @param ground The type of ground that will be randomly placed on the map
+	 * @param odds The odds of a successful placement
 	 */
-	public static void addSprouts(List<String> map) {
+	public static void generateRandomGrounds(List<String> map, Ground ground, double odds) {
 		for (int i = 0; i <  map.size(); i++) {
+			char[] newString = map.get(i).toCharArray();
 			for (int j = 0; j < map.get(i).length(); j++) {
-				if (Math.random() < 0.01 && map.get(i).charAt(j) == '.') {
-					char[] newString = map.get(i).toCharArray();
-					newString[j] = '+';
-					map.set(i, String.valueOf(newString));
+				if (Math.random() < odds && map.get(i).charAt(j) == '.') {
+					newString[j] = ground.getDisplayChar();
 				}
 			}
+			map.set(i, String.valueOf(newString));
 		}
 	}
 }
