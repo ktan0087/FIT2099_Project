a059c70e2bf28b2407c62a8e2a45604efb3a4cbe
dong0009 <dong0009@student.monash.edu>
2022-05-10 18:53:09 +0800

    Modify Fountain class to have limited water

--- src/game/grounds/Fountain.java
+++ src/game/grounds/Fountain.java
@@ -2,9 +2,14 @@ package game.grounds;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
 import game.enums.Status;
 
 public abstract class Fountain extends Ground {
+    private static final int MAX_CAPACITY = 10;
+    private int slot;
+    private int rechargeTimer = 5;
+
     /**
      * Constructor.
      *
@@ -12,10 +17,35 @@ public abstract class Fountain extends Ground {
      */
     public Fountain(char displayChar) {
         super(displayChar);
+        this.slot = MAX_CAPACITY;
     }
 
-    @Override
-    public boolean canActorEnter(Actor actor) {
-        return !actor.hasCapability(Status.HOSTILE_TO_PLAYER);
+    public int getSlot(){
+        return slot;
     }
+
+    public void subtractSlot(){
+        slot--;
+    }
+
+    private void rechargeFountain(){
+        slot = MAX_CAPACITY;
+        rechargeTimer = 5;
+    }
+
+    public int getMaxCapacity(){
+        return MAX_CAPACITY;
+    }
+
+
+    @Override
+    public void tick(Location location) {
+        if (slot == 0){
+            rechargeTimer--;
+            if (rechargeTimer < 0){
+                rechargeFountain();
+            }
+        }
+    }
+
 }
