d84dfbacdd8837ac26feafc4c05eb2326793425c
dong0009 <dong0009@student.monash.edu>
2022-05-10 19:04:32 +0800

    Subtract slots after refill

--- src/game/actions/RefillAction.java
+++ src/game/actions/RefillAction.java
@@ -3,25 +3,30 @@ package game.actions;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.grounds.Fountain;
 import game.items.Bottle;
 import game.waters.Water;
 
 public class RefillAction extends Action {
     private Water water;
+    private Fountain fountain;
 
-    public RefillAction(Water water){
+    public RefillAction(Water water, Fountain fountain){
         this.water = water;
+        this.fountain = fountain;
     }
 
     @Override
     public String execute(Actor actor, GameMap map) {
         Bottle.fillWater(water);
 
-        return menuDescription(actor);
+        fountain.subtractSlot();
+
+        return actor + " refill " + water;
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return actor + " refill " + water;
+        return actor + " refills bottle from " + fountain + " (" + fountain.getSlot() + "/" + fountain.getMaxCapacity() + ")";
     }
 }
