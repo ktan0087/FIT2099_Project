7ec495b2fdde1291163e42f9a71ca44e1e3bc082
dong0009 <dong0009@student.monash.edu>
2022-05-11 09:11:48 +0800

    Modify Water class to extend from Item class and implement Consumable interface

--- src/game/waters/Water.java
+++ src/game/waters/Water.java
@@ -1,7 +1,17 @@
 package game.waters;
 
-import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import game.interfaces.Consumable;
 
-public abstract class Water {
-    public abstract void drink(Actor actor);
+public abstract class Water extends Item implements Consumable {
+    /***
+     * Constructor.
+     *  @param name the name of this Item
+     * @param displayChar the character to use to represent this item if it is on the ground
+     * @param portable true if and only if the Item can be picked up
+     */
+    public Water(String name, char displayChar, boolean portable) {
+        super(name, displayChar, portable);
+    }
+
 }
