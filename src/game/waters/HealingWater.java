83ccbc5b7181d04d8cfc6c0ca7a58c11f2b6ac59
dong0009 <dong0009@student.monash.edu>
2022-05-11 09:13:18 +0800

    Added a constructor and modify the drink method to be consumeMagicalItems method

--- src/game/waters/HealingWater.java
+++ src/game/waters/HealingWater.java
@@ -4,10 +4,19 @@ import edu.monash.fit2099.engine.actors.Actor;
 
 public class HealingWater extends Water {
     private static final String NAME = "Healing water";
+    private static final char DISPLAY_CHAR = '~';
     private static final int HEAL_HITPOINT = 50;
 
+    /***
+     * Constructor.
+     */
+    public HealingWater() {
+        super(NAME, DISPLAY_CHAR, false);
+    }
+
+
     @Override
-    public void drink(Actor actor) {
+    public void consumeMagicalItems(Actor actor) {
         actor.heal(HEAL_HITPOINT);
     }
 
@@ -15,4 +24,5 @@ public class HealingWater extends Water {
     public String toString() {
         return NAME;
     }
+
 }
