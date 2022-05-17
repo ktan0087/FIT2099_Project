bdcea2837ae1961526a74d1fd0b3e80ec1cb38fb
Kennedy Tan <ktan0087@student.monash.edu>
2022-05-17 13:56:55 +0800

    override tick method to check if its is burning

--- src/game/grounds/Dirt.java
+++ src/game/grounds/Dirt.java
@@ -1,6 +1,9 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
 import game.enums.Status;
 
 /**
@@ -11,12 +14,42 @@ import game.enums.Status;
  */
 public class Dirt extends Ground {
 
+	//TODO: when ground is burning
+	/**
+	 * Number of turns to keep the ground on fire
+	 */
+	private int burningTurn = 3;
+	/**
+	 * Create display object to print message to console
+	 */
+	Display display = new Display();
+
 	/**
 	 * A constructor for the Dirt class
 	 */
 	public Dirt() {
 		super('.'); // display character
 		this.addCapability(Status.FERTILE); // add fertile status to Dirt grounds
+	}
+
+	@Override
+	public void tick(Location location){
+		if (location.getGround().hasCapability(Status.IS_BURNING) && burningTurn > 0){
+			if (location.containsAnActor()){
+				edu.monash.fit2099.engine.actors.Actor target = location.getActor();
+				if (target.hasCapability(Status.HOSTILE_TO_ENEMY) || target.hasCapability(Status.HOSTILE_TO_ENEMY)){
+					display.println(target + " is on fire, receives 20 damage! ");
+					target.hurt(20);
+				}
+			}
+			burningTurn--;
+		}
+		else if (location.getGround().hasCapability(Status.IS_BURNING) && burningTurn == 0){
+			location.getGround().removeCapability(Status.IS_BURNING);
+			display.println(this + " has stopped burning ");
+			burningTurn = 3;
+		}
+		super.tick(location);
 	}
 
 	/**
@@ -27,4 +60,5 @@ public class Dirt extends Ground {
 	public String toString() {
 		return "Dirt";
 	}
+
 }
