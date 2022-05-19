afd7043edf5c794e512f0af44d61be86c6d09ba5
dong0009 <dong0009@student.monash.edu>
2022-05-19 22:14:35 +0800

    Added JavaDoc for Luigi class

--- src/game/actors/Luigi.java
+++ src/game/actors/Luigi.java
@@ -14,10 +14,26 @@ import game.interfaces.Resettable;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A Luigi class which acts as a patrol unit
+ *
+ * @author Ong Di Sheng
+ * @version 1.0
+ */
 public class Luigi extends Actor implements Resettable {
+    /**
+     * The name is set as a constant called Luigi
+     */
     private static final String NAME = "Luigi";
+
+    /**
+     * The display character is set as a constant @
+     */
     private static final char DISPLAY_CHAR = '@';
 
+    /**
+     * The hash map storing the behaviours with priorities
+     */
     private final Map<Integer, Behaviour> behaviours = new HashMap<>(); // priority, behaviour
 
     /**
@@ -29,6 +45,15 @@ public class Luigi extends Actor implements Resettable {
         this.registerInstance();
     }
 
+    /**
+     * Luigi will just patrol in each turn
+     *
+     * @param actions    collection of possible Actions for this Actor
+     * @param lastAction The Action this Actor took last turn. Can do interesting things in conjunction with Action.getNextAction()
+     * @param map        the map containing the Actor
+     * @param display    the I/O object to which messages may be written
+     * @return Action either from PatrolBehaviour or DoNothingAction (reset)
+     */
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
         if (this.hasCapability(Status.RESET)){
@@ -36,10 +61,12 @@ public class Luigi extends Actor implements Resettable {
 
             int xCoord = map.locationOf(this).x();
 
+            // if the reset location contains an actor, then remove it
             if (map.at(map.locationOf(this).x(), PatrolBehaviour.getMaxYCoord()).containsAnActor()){
                 map.removeActor(map.at(map.locationOf(this).x(), PatrolBehaviour.getMaxYCoord()).getActor());
             }
 
+            // move Luigi to reset location
             map.moveActor(this, map.at(xCoord, PatrolBehaviour.getMaxYCoord()));
             display.println(this + " moves back to original position (" + xCoord + ", " + PatrolBehaviour.getMaxYCoord() + ")");
             return new DoNothingAction();
@@ -56,6 +83,9 @@ public class Luigi extends Actor implements Resettable {
         return new DoNothingAction();
     }
 
+    /**
+     * Add RESET status to Luigi during reset
+     */
     @Override
     public void resetInstance() {
         this.addCapability(Status.RESET);
