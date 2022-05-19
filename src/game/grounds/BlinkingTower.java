f531cbad688a18c102798cc40127e0cecc8db715
dong0009 <dong0009@student.monash.edu>
2022-05-19 22:14:24 +0800

    Added JavaDoc for BlinkingTower class

--- src/game/grounds/BlinkingTower.java
+++ src/game/grounds/BlinkingTower.java
@@ -6,13 +6,20 @@ import edu.monash.fit2099.engine.positions.Location;
 import game.actions.BlinkAction;
 import game.enums.Status;
 
+/**
+ * A BlinkingTower class that allows the player to blink to several steps away
+ *
+ * @author Ong Di Sheng
+ * @version 1.0
+ *
+ */
 public class BlinkingTower extends HighGround {
     /**
-     * The jump chance related to a WarpPipe (used for jumping)
+     * The jump chance related to a BlinkingTower (used for jumping)
      */
     private static final double JUMP_CHANCE = 1.0;
     /**
-     * The fall damage related to a WarpPipe (used for jumping)
+     * The fall damage related to a BlinkingTower (used for jumping)
      */
     private static final int FALL_DAMAGE = 0;
 
@@ -23,9 +30,17 @@ public class BlinkingTower extends HighGround {
         super('?');
         setJumpChance(JUMP_CHANCE);
         setFallDamage(FALL_DAMAGE);
-        this.addCapability(Status.BLINKABLE);
+        this.addCapability(Status.BLINKABLE);   // add BLINKABLE status
     }
 
+    /**
+     * Allow the player to blink if the player steps on this blinking tower
+     *
+     * @param actor the Actor performing an action
+     * @param location the current Location of the High Ground
+     * @param direction the direction of the High Ground from the Actor
+     * @return BlinkAction if the player steps on this blinking tower
+     */
     @Override
     public ActionList allowableActions(Actor actor, Location location, String direction) {
         ActionList actions = super.allowableActions(actor, location, direction);
@@ -35,6 +50,11 @@ public class BlinkingTower extends HighGround {
         return actions;
     }
 
+    /**
+     * Return the name of this blinking tower
+     *
+     * @return the name of this blinking tower
+     */
     @Override
     public String toString() {
         return "Blinking Tower";
