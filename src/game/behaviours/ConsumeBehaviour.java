5ca3e5d726c0a6d28a02fec8729b28c11940fa74
dong0009 <dong0009@student.monash.edu>
2022-05-19 21:07:04 +0800

    Added JavaDoc for ConsumeBehaviour class

--- src/game/behaviours/ConsumeBehaviour.java
+++ src/game/behaviours/ConsumeBehaviour.java
@@ -9,15 +9,31 @@ import game.interfaces.Behaviour;
 import game.waters.HealingWater;
 import game.waters.PowerWater;
 
+/**
+ * A ConsumeBehaviour class which is responsible for the enemies to consume either PowerWater or HealingWater
+ *
+ * @author Ong Di Sheng
+ * @version 1.0
+ */
 public class ConsumeBehaviour implements Behaviour {
+    /**
+     * Get ConsumeAction if enemies step on either PowerFountain or HealthFountain
+     *
+     * @param actor the Actor acting
+     * @param map the GameMap containing the Actor
+     * @return ConsumeAction if enemies step on either PowerFountain or HealthFountain
+     */
     @Override
     public Action getAction(Actor actor, GameMap map) {
+        // if current location of enemy is PowerFountain, then consume PowerWater
         if (map.locationOf(actor).getGround().hasCapability(Status.POWER)){
             return new ConsumeAction(new PowerWater());
         }
+        // if current location of enemy is HealingFountain, then consume HealingWater
         else if (map.locationOf(actor).getGround().hasCapability(Status.HEAL)){
             return new ConsumeAction(new HealingWater());
         }
+        // otherwise, null
         return null;
     }
 }
