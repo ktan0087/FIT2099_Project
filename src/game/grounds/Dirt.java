d90952e9e3dd5bca9fd37937ab1a240a7ae8d5c9
Kennedy Tan <ktan0087@student.monash.edu>
2022-05-21 21:58:06 +0800

    remove condition from dirt to check if the ground is burning

--- src/game/grounds/Dirt.java
+++ src/game/grounds/Dirt.java
@@ -1,6 +1,5 @@
 package game.grounds;
 
-import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
@@ -9,24 +8,34 @@ import game.enums.Status;
 /**
  * A class that represents bare dirt.
  * @author Mark Manlangit
- * @version 1.0
+ * @editor Kennedy Tan
+ * @version 2.0
  * @since 01-05-2022
  */
 public class Dirt extends Ground {
 
-	//TODO: when ground is burning
-	/**
+/*	*//**
 	 * Number of turns to keep the ground on fire
-	 */
+	 *//*
 	private int burningTurn = 3;
-	/**
+	*//**
+	 * Number of turns to start to let the player on fire
+	 *//*
+	private int startBurningTurn = 4;
+	*//**
 	 * Create display object to print message to console
-	 */
+	 *//*
 	Display display = new Display();
+	*//**
+	 * Character to display fire
+	 *//*
+	private static final char fireChar = 'v';*/
 
-	private static final char fireChar = 'v';
-
+	/**
+	 * Character to display dirt
+	 */
 	private static final char dirtChar = '.';
+
 	/**
 	 * A constructor for the Dirt class
 	 */
@@ -35,27 +44,43 @@ public class Dirt extends Ground {
 		this.addCapability(Status.FERTILE); // add fertile status to Dirt grounds
 	}
 
+/*	*//**
+	 * Override tick method to check the ground every turn
+	 *
+	 * @param location The location of the Ground
+	 *//*
 	@Override
 	public void tick(Location location){
+		//check if the ground is burning
 		if (location.getGround().hasCapability(Status.IS_BURNING) && burningTurn > 0){
+			//set character to 'v'
 			this.setDisplayChar(fireChar);
+			//check if the location contains actor
 			if (location.containsAnActor()){
 				edu.monash.fit2099.engine.actors.Actor target = location.getActor();
-				if (target.hasCapability(Status.HOSTILE_TO_ENEMY) || target.hasCapability(Status.HOSTILE_TO_PLAYER)){
+				//check if target can be hurt by fire
+				if ((target.hasCapability(Status.HOSTILE_TO_ENEMY) || target.hasCapability(Status.HOSTILE_TO_PLAYER)) && startBurningTurn <= 3){
+					//display message to console
 					display.println(target + " is on fire, receives 20 damage! ");
 					target.hurt(20);
 				}
 			}
 			burningTurn--;
+			startBurningTurn--;
 		}
+		//check if ground is burning and burningTurn is zero
 		else if (location.getGround().hasCapability(Status.IS_BURNING) && burningTurn == 0){
+			//remove burning capability from ground
 			location.getGround().removeCapability(Status.IS_BURNING);
+			//display stop burning message to console
 			display.println(this + " has stopped burning ");
 			burningTurn = 3;
+			startBurningTurn = 4;
+			//set character back to '.'
 			this.setDisplayChar(dirtChar);
 		}
 		super.tick(location);
-	}
+	}*/
 
 	/**
 	 * A method used to return the string representation of the Dirt class
@@ -65,5 +90,4 @@ public class Dirt extends Ground {
 	public String toString() {
 		return "Dirt";
 	}
-
 }
